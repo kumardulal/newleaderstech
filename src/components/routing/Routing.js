c9bb24a8e139f7573ce2008b82597631c155b139
KD <48119415+kumardulal@users.noreply.github.com>
2021-09-20 02:58:46 +0800

    Update Routing.js

--- src/components/routing/Routing.js
+++ src/components/routing/Routing.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Homepage from '../../pages/Homepage';
 // import ContentContainer from '../navigation/ContentContainer';
 
